5df7d09620748e431f73a918ad9e6cdd4841b23f
Alexsandr <pererodovsasha2362000@gmail.com>
2023-05-30 11:29:50 +0300

    first commit

---
+++ ind/script.js
@@ -0,0 +1,80 @@
+"use strict";
+/*
+let numberOrFilms = prompt("Сколько фильмов вы уже посмотрели?", " ");
+
+const personalMovieDB = {
+    count: numberOrFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренніх фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренніх фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+*/
+/*
+if (1) {
+    console.log('ok!');
+} else {
+    console.log("error");
+}
+*/
+
+/*
+if ( num < 49) {
+    console.log("error");
+} else if (num > 100) {
+    console.log("big");
+} else {
+    console.log("ok");
+}
+
+(num === 50) ? console.log("ok") : console.log("error");
+*/
+
+/*
+switch(num) {
+    case 49: 
+        console.log("ERROR ");
+        break;
+    case 100: 
+        console.log("ERROR ");
+        break;
+    case 50: 
+        console.log("good");
+        break;
+    default:
+        console.log("Не в єтот раз");
+        break;
+}
+*/
+/*
+let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+do {
+    console.log(num);
+    num++;
+} while (num<55);
+
+for(let i = 1; i < 10; i++) {
+    if(i === 6) {
+        //break;
+        continue;
+    }
+    console.log(i);
+    
+} 
+*/
